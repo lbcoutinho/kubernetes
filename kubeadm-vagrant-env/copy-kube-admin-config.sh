db33253cfb27aba92a54673f317136fbc39ba098
Leandro Coutinho <leandro.coutinho@somosdx.co>
2020-07-27 23:35:42 -0300

    Improvements to kubeadm-vagrant-env
     * Set /etc/hosts and install kubectl in copy-kube-admin-config.sh
     * Remove trigger from Vagrantfile

--- kubeadm-vagrant-env/copy-kube-admin-config.sh
+++ kubeadm-vagrant-env/copy-kube-admin-config.sh
@@ -2,9 +2,29 @@
 
 echo "Copying kube admin config from kmaster to local machine..."
 
+# Set cluster nodes address at local /etc/hosts
+if ! grep -q kmaster /etc/hosts; then
+echo "Setting nodes hosts in /etc/hosts..."
+sudo cat >> /etc/hosts <<EOF
+172.27.0.100 kmaster.example.com kmaster
+172.27.0.101 kworker1.example.com kworker1
+172.27.0.102 kworker2.example.com kworker2
+EOF
+fi
+
+# Install kubectl
+if ! which kubectl; then
+  echo "Installing kubectl..."
+  curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" > /dev/null 2>&1
+  chmod +x ./kubectl
+  sudo mv ./kubectl /usr/local/bin/kubectl
+fi
+
 mkdir -p $HOME/.kube
+echo "Installing sshpass..."
 sudo apt install -y -q sshpass > /dev/null 2>&1
-sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@kmaster.example.com:/home/vagrant/.kube/config $HOME/.kube/config 2>/dev/null
+echo "Copying Kubernetes admin.conf..."
+sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@kmaster.example.com:/etc/kubernetes/admin.conf $HOME/.kube/config
 echo "Done!"
 
 kubectl get nodes
